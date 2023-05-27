ed151b55ff1e018c187b1f2dea82c2c3fc593df5
Jim Brazeal <jim.brazeal@kellogg.northwestern.edu>
2023-05-27 14:56:57 +0000

    Session creation and authentication (no hash) - 1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,36 @@
 class SessionsController < ApplicationController
   def new
+
   end
 
   def create
+
+    # Find the user based on the email the user entered
+    @user = User.find_by({"email" => params["email"]})
+
+    if @user != nil
+      # Check to see if the password entered by the user is the same we have on file
+      # If the passwords don't match, allow them to try again
+      if @user["password"] != params["password"]
+        flash["notice"] = "Wrong Password. Try Again."
+        redirect_to "/sessions/new"
+        # If the passwords match, send users to the places file
+      else
+        flash["notice"] = "Welcome!"
+        redirect_to "/places"
+      end 
+    # If the user does not exist based on the email, tell them to create an account
+    else
+      flash["notice"] = "User does not exist. Please create an account."
+      redirect_to "/sessions/new"
+    end
+
   end
 
   def destroy
+    #logout the user
+    flash["notice"] = "Goodbye!"
+    redirect_to "/sessions/new"
   end
 end
   
