8ffd8f099d477e97c6fbf29a7ab91ef3204811db
Jim Brazeal <jim.brazeal@kellogg.northwestern.edu>
2023-05-30 16:59:32 +0000

    Syncing for updated filepath

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,24 +13,26 @@ class SessionsController < ApplicationController
       # If the passwords don't match, allow them to try again
       if BCrypt::Password.new(@user["password"]) != params["password"]
         flash["notice"] = "Wrong Password. Try Again."
-        redirect_to "/sessions/new"
+        redirect_to "/login"
         # If the passwords match, send users to the places file
       else
+        session["user_id"] = @user["id"]
         flash["notice"] = "Welcome!"
         redirect_to "/places"
       end 
     # If the user does not exist based on the email, tell them to create an account
     else
       flash["notice"] = "User does not exist. Please create an account."
-      redirect_to "/sessions/new"
+      redirect_to "/login"
     end
 
   end
 
   def destroy
     #logout the user
+    session["user_id"] = nil
     flash["notice"] = "Goodbye!"
-    redirect_to "/sessions/new"
+    redirect_to "/login"
   end
 end
   
